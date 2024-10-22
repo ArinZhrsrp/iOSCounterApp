f54adfaab888e8cfc72d11edb74e28c9f3a9004f
Shirin Zahra <shirin@bayo.com.my>
2024-10-22 17:03:43 +0800

    Initial Commit

---
+++ iOSCounterApp/iOSCounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCounterAppApp.swift
+//  iOSCounterApp
+//
+//  Created by Shirin on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSCounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSCounterAppTests/iOSCounterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCounterAppTests.swift
+//  iOSCounterAppTests
+//
+//  Created by Shirin on 22/10/2024.
+//
+
+import Testing
+@testable import iOSCounterApp
+
+struct iOSCounterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
